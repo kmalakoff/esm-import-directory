d311e6e697e5077ebdd63af5c8cf173f459cb710
Kevin Malakoff <kmalakoff@gmail.com>
2017-11-26 16:56:48 -0800

    Add recursive walking

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
+esm-require-directory
+------------
+
 Require a directory of .mjs module defaults using @std/esm in Node.js
 
-Usage
-=============
+**Usage**
 
 ```js
 import path from 'path';
@@ -9,11 +11,18 @@ import importDirectory from 'esm-import-directory';
 
 const dirname = path.dirname(import.meta.url.replace('file://', ''));
 
-export default async (options) => {
+(async () => {
   // import as array, eg. [{ hello: 'world' }]
   const typeDefs = await importDirectory(path.join(dirname, 'typeDefs'));
 
   // import with paths, eg. { 'filename.mjs': { hello: 'world' } }
   const typeDefPaths = await importDirectory(path.join(dirname, 'typeDefs'), { paths: true });  
-};
+})();
 ```
+
+**Options**
+
+- recursive (boolean) - traverse modules recursively. Default: false.
+- paths (boolean) - modules returned as an object with relative paths vs as an array. Default: false.
+- default (boolean) - extract default from es6 modules. Default: true.
+- extensions (array) - the file extension types to process. Default: ['.mjs', '.js']
--- index.mjs
+++ index.mjs
@@ -1,22 +1,50 @@
 import path from 'path';
 import fs from 'fs';
 import pify from 'pify';
+import walk from 'walk-filtered';
 
-const readdir = pify(fs.readdir);
+const pWalk = pify(walk);
+const EXTENSIONS = ['.mjs', '.js'];
 
 export default async (directory, options={}) => {
-  const fileNames = await readdir(directory);
+  const extensions = options.extensions || EXTENSIONS;
 
-  const modules = options.paths ? {} : [];
-  for (const fileName of fileNames) {
-    const fullPath = path.join(directory, fileName);
-    try {
-      const module = await import(fullPath);
-      if (module.default) {
-        if (options.paths) modules[fileName] = module.default;
-        else modules.push(module.default);
+  // collect all the module paths
+  const relativePaths = [];
+  await pWalk(directory, (relativePath, stats) => {
+    if (relativePath === '') return true;
+    if (stats.isDirectory()) {
+      if (options.recursive) return true; // traverse directories
+      
+      // check for index file one level under the directory
+      for (const extension of extensions) {
+        const relativeIndexPath = path.join(relativePath, `index${extension}`);
+        try {
+          const indexStats = fs.statSync(path.join(directory, relativeIndexPath));
+          if (indexStats.isFile()) {
+            relativePaths.push(relativeIndexPath);
+            break;
+          }
+        } catch (err) { /* no index */ }
       }
-    } catch (err) { /* not a module */ }
-  }
-  return modules;
+      return false; // do not traverse directories
+    }
+    if (~extensions.indexOf(path.extname(relativePath))) relativePaths.push(relativePath);
+  }, true);
+
+  // resolve all the module paths
+  const results = options.paths ? {} : [];
+  await Promise.all(relativePaths.map(async (relativePath) => {
+    let module = await import(path.join(directory, relativePath));
+
+    // es6 module so extract default by default unless asked not to
+    if (path.extname(relativePath) === '.mjs') {
+      if (!options.hasOwnProperty('default') || options.default) module = module.default;
+    }
+
+    // store result
+    if (options.paths) results[relativePath] = module;
+    else results.push(module);
+  }));
+  return results;
 }
