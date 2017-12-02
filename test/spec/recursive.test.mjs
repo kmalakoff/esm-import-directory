be2d5adc0ff5bab43c79652278c24eea7a8b9981
Kevin Malakoff <kmalakoff@gmail.com>
2017-12-02 10:42:53 -0800

    Added missing tests

---
+++ test/spec/recursive.test.mjs
@@ -0,0 +1,19 @@
+import path from 'path';
+import { assert } from 'chai';
+
+import importDirectory from '../..';
+
+const dirname = path.dirname(import.meta.url.replace('file://', ''));
+const DATA_DIRECTORY = path.join(dirname, '..', 'data', 'directory');
+
+describe('recursive', () => {
+  it('recursive: false', async () => {
+    const results = await importDirectory(DATA_DIRECTORY, { recursive: false });
+    assert.equal(results.length, 1);
+  });
+
+  it('recursive: true', async () => {
+    const results = await importDirectory(DATA_DIRECTORY, { recursive: true });
+    assert.equal(results.length, 5);
+  });
+});
