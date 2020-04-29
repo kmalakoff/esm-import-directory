a077ac7fc0d3b609e52fc05b228afe227d4db1b2
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-28 18:08:13 -0700

    Use url.fileURLToPath to resolve __dirname in tests

--- test/spec/recursive.test.mjs
+++ test/spec/recursive.test.mjs
@@ -1,11 +1,12 @@
 import path from 'path';
+import url from 'url';
 import chai from 'chai';
 
 import importDirectory from '../../index.mjs';
 
 const { assert } = chai;
-const dirname = path.dirname(import.meta.url.replace('file://', ''));
-const DATA_DIRECTORY = path.join(dirname, '..', 'data', 'directory');
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const DATA_DIRECTORY = path.join(__dirname, '..', 'data', 'directory');
 
 describe('recursive', () => {
   it('recursive: false', async () => {
