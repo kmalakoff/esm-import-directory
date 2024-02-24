3eb74f133b474e98e1fef988ba400ec90d86332b
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-24 06:44:31 -0800

    Upgrade dependencies

--- dist/cjs/index.esm.js
+++ dist/cjs/index.esm.js
@@ -15,9 +15,4 @@ function _interop_require_default(obj) {
     };
 }
 var _default = _esmrequiredirectory.default;
-
-if ((typeof exports.default === 'function' || (typeof exports.default === 'object' && exports.default !== null)) && typeof exports.default.__esModule === 'undefined') {
-  Object.defineProperty(exports.default, '__esModule', { value: true });
-  for (var key in exports) exports.default[key] = exports[key];
-  module.exports = exports.default;
-}
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,1 +1,2 @@
 export = requireDirectory;
+declare var requireDirectory: any;
