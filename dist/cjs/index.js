3a67dbe252c35a180ab13ec110e0bd3d05ab517b
Kevin Malakoff <kmalakoff@gmail.com>
2024-11-24 12:24:45 -0800

    Upgrade modules

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -1,4 +1,4 @@
 "use strict";
-var requireDirectory = require("esm-require-directory");
+var requireDirectory = require('esm-require-directory');
 module.exports = requireDirectory;
 /* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
