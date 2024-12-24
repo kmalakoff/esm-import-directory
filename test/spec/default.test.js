deb8b470785c18c6f08acb4ec9b2a4aa1af37364
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 09:47:14 -0800

    Upgrade modules

--- test/spec/default.test.js
+++ test/spec/default.test.js
@@ -8,7 +8,7 @@ var DATA_DIR = path.join(__dirname, '..', 'data', 'directory');
 describe('default', function () {
   it('default: true, recursive: false', function (done) {
     requireDirectory(DATA_DIR, { default: true, recursive: false }, function (err, results) {
-      assert.ok(!err);
+      assert.ok(!err, err ? err.message : '');
       assert.equal(results.length, 2);
       done();
     });
@@ -16,7 +16,7 @@ describe('default', function () {
 
   it('default: true, recursive: true', function (done) {
     requireDirectory(DATA_DIR, { default: true, recursive: true }, function (err, results) {
-      assert.ok(!err);
+      assert.ok(!err, err ? err.message : '');
       assert.equal(results.length, 10);
       done();
     });
@@ -24,7 +24,7 @@ describe('default', function () {
 
   it('default: false, recursive: false', function (done) {
     requireDirectory(DATA_DIR, { default: false, recursive: false }, function (err, results) {
-      assert.ok(!err);
+      assert.ok(!err, err ? err.message : '');
       assert.equal(results.length, 2);
       done();
     });
@@ -32,7 +32,7 @@ describe('default', function () {
 
   it('default: false, recursive: true', function (done) {
     requireDirectory(DATA_DIR, { default: false, recursive: true }, function (err, results) {
-      assert.ok(!err);
+      assert.ok(!err, err ? err.message : '');
       assert.equal(results.length, 10);
       done();
     });
