efcf8e1382f36aa0f5cdd44896e8deaf3996d6e1
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-17 23:41:24 -0700

    Update Biome configuration and CI node version

--- test/spec/defaultOptions.test.ts
+++ test/spec/defaultOptions.test.ts
@@ -1,9 +1,9 @@
 import assert from 'assert';
-import path from 'path';
-import url from 'url';
 // @ts-ignore
 import importDirectory from 'esm-import-directory';
+import path from 'path';
 import Pinkie from 'pinkie-promise';
+import url from 'url';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 const DATA_DIR = path.join(__dirname, '..', 'data', 'directory');
